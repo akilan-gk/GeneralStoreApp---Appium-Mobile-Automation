0443af33281d16dcfe329552f0a0d7a6b52bff88
Akilan GK <akilangk30@gmail.com>
2022-03-28 09:59:39 +0530

    SecondCommit

--- src/main/java/generalStoreMobileAutomation/testScripts/AddingProductToCartTest.java
+++ src/main/java/generalStoreMobileAutomation/testScripts/AddingProductToCartTest.java
@@ -4,6 +4,10 @@ import generalStoreMobileAutomation.reUsables.ActionReUsables;
 import generalStoreMobileAutomation.suiteFile.GeneralStoreBase;
 import io.appium.java_client.android.AndroidElement;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -22,13 +26,18 @@ public class AddingProductToCartTest extends GeneralStoreBase {
         logger.info("Opening the cart");
         ActionReUsables.click(ActionReUsables.findElement(driver, "ID", locatorsPropertyObject.getProperty("cart")));
         logger.info("Verifying that the added products in the cart are same as the user's choice");
-        List<AndroidElement> productNameElements = ActionReUsables.findElements(driver, "ID", locatorsPropertyObject.getProperty("productNames"));
-        for (AndroidElement product : productNameElements) {
-            String name = ActionReUsables.getText(product);
+        List<WebElement> productNameElements =waitForElement(locatorsPropertyObject.getProperty("productNames"));
+        //List<AndroidElement> productNameElements = ActionReUsables.findElements(driver, "ID", locatorsPropertyObject.getProperty("productNames"));
+        for (WebElement product : productNameElements) {
+            String name = ActionReUsables.getText((AndroidElement) product);
             boolean result = name.equals(productOne) || name.equals(productTwo);
             Assert.assertTrue(result, "Product Mismatch: " + name);
             logger.info("Products Matched");
         }
+    }
+    public List<WebElement> waitForElement(String locator){
+        WebDriverWait wait = new WebDriverWait(driver,3);
+        return wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id(locator)));
     }
 }
 
